25189d481173137ef54888fbdc6a6e8e3b23ab61
eindreis <46956058+eindreis@users.noreply.github.com>
2021-04-18 12:34:25 +0200

    Add files via upload

---
+++ Data/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRM.Data
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; } = 0;
+
+        [Required]
+        public string Phone { get; set; } = "";
+
+        [Required]
+        public string Name { get; set; } = "";
+
+        [Required]
+        public string Email { get; set; } = "";
+
+        [Required]
+        public string EmploymentDate { get; set; } = "";
+    }
+}
